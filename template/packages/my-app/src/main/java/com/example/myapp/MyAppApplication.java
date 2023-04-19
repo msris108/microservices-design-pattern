e15d8774930840f4f4abf18ee4d5209e44f06713
Sriram M <srim2@publicisgroupe.net>
2023-04-19 15:14:14 +0530

    Add template and service-registry

---
+++ template/packages/my-app/src/main/java/com/example/myapp/MyAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class MyAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyAppApplication.class, args);
+  }
+}
